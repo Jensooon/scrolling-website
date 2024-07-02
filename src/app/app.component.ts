8fbee075ea65cd3fff3496db5b4ba47a5e832b76
Jensooon <jenson.wintle@gmail.com>
2024-07-02 20:16:11 +0100

    Added scrolling functionality, still need to fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  QueryList,
+  Renderer2,
+  ViewChildren,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +12,32 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'scrolling-website';
+
+  @ViewChildren('hiddenElement') hiddenElements!: QueryList<ElementRef>;
+
+  observer: IntersectionObserver;
+
+  constructor(private renderer: Renderer2) {
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          this.renderer.addClass(entry.target, 'visible');
+        } else {
+          this.renderer.removeClass(entry.target, 'visible');
+        }
+      });
+    });
+  }
+
+  ngOnInit() {
+    this.hiddenElements.changes.subscribe((elements: QueryList<ElementRef>) => {
+      elements.forEach((element) =>
+        this.observer.observe(element.nativeElement)
+      );
+    });
+  }
 }
